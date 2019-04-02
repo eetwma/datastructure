4c70570d01fbb532922b00330556620cfceb8d10
eetwma <eetwma@gmail.com>
2019-04-02 15:53:04 -0700

    revise

--- bsearch.java
+++ bsearch.java
@@ -16,7 +16,7 @@ class bsearch {
         return -1; 
     } 
 
-    public static void main(String args[]) 
+    public static void main(String[] args) 
     { 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int l = arr.length; 
---
+++ selectionsort.java
@@ -0,0 +1,34 @@
+public class Selectionsort {
+    public static int[] ssort(int[] array) {
+      int n = array.length;
+      for(int i = 0;i<n;i++)
+      {
+        int min = i;
+        for(int j =i+1;j<n;j++)
+        {
+          if(array[j]<array[min])
+          {
+            min = j;
+          }
+        }
+        int temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+      }
+    }
+    public static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
+    public static void main(String args[]) 
+    { 
+        int[] array = { 5, 4, 6, 1, 3, 2, 7, 8, 9 }; 
+        ssort(array);
+        printArray(array);
+    } 
+  }
+
+  
