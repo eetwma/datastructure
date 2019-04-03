abc7e1df687a730f36160a5198a2273deb014b88
eetwma <eetwma@gmail.com>
2019-04-03 10:20:31 -0700

    change

--- getarrsize.java
+++ getarrsize.java
@@ -1,7 +1,7 @@
 public class getarrsize{
     public static void main(String[] args){
         int n = 10;
-        int[] arr = new int[10];
+        int[] arr = new int[n];
         System.out.println(arr.length);
     }
 }
