0843b360ec6d249b523bed3d501a792edb067bbc
eetwma <eetwma@gmail.com>
2019-04-03 16:12:45 -0700

    htable change

--- hashtable.java
+++ hashtable.java
@@ -13,11 +13,20 @@ class hashtable {
         h.put(3, "We"); 
         h.put(2, "SSCRG"); 
         h.put(1, "isBest"); 
-  
+        
         // create a clone or shallow copy of hash table h 
         h1 = (Hashtable<Integer, String>)h.clone(); 
+
         //get the value by using the key
         System.out.println("values of key 3 is: " + h.get(3));
+
+        // Checking for the Value 'Geeks' 
+        System.out.println("Is the value 'Geeks' present? " +  
+                            h.contains("SSCRG"));
+
+        // Checking for the Key '3' 
+        System.out.println("Is the key '3' contained? " +  
+        h.containsKey(3));  
 
         // checking clone h1 
         System.out.println("values in clone: " + h1); 
@@ -27,6 +36,11 @@ class hashtable {
   
         // checking hash table h 
         System.out.println("after clearing: " + h); 
+
+        // checking whether hash table h is empty or not 
+        if (h.isEmpty()) {
+            System.out.println("yes hash table is empty"); 
+    } 
 
         
     } 
